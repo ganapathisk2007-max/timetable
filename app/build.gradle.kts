56adeb748cf7f2a21c1127e43fb31b0cc51f0fae
heftymouse <59918974+heftymouse@users.noreply.github.com>
2026-01-25 13:36:39 +0530

    begin 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     applicationId = "town.amrita.timetable"
     minSdk = 29
     targetSdk = 36
-    versionCode = 12
-    versionName = "1.1.1"
+    versionCode = 13
+    versionName = "1.1.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
